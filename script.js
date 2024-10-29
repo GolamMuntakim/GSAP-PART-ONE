d83f92d40deb19fc5c7eead792e01a38e868ef35
Golam Muntakim <seams123456789@gmail.com>
2024-10-28 23:11:45 -0700

    gsap explore

---
+++ script.js
@@ -0,0 +1,21 @@
+let tl = gsap.timeline()
+tl.from("h2",{
+    y:-30,
+    opacity:0,
+    duration:1,
+    delay:0.5
+
+})
+tl.from("h4",{
+    y:-20,
+    opacity:0,
+    duration:1,
+    delay:0.5,
+    stagger:0.3
+}).
+tl.from("h1",{
+    y:20,
+    opacity:0,
+    duration:1,
+    scale:0.2
+})
